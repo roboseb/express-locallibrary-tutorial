0ea5134713df5b8261db49ea88c0de3efc881230
roboseb <sebastienapril@rocketmail.com>
2022-09-02 15:33:33 -0600

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://mdn:mdn@cluster0.fjfxvnv.mongodb.net/local_library?retryWrites=true&w=majority';
+
+// Set up mongoose connection
+const dev_db_url = 'mongodb+srv://mdn:mdn@cluster0.fjfxvnv.mongodb.net/local_library?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
